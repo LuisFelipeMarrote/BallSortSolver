900a0ad98c1f565a991d094f075a590cd61055ad
LuisFelipeMarrote <luisfelipe.marrote@gmail.com>
2024-07-08 01:45:56 -0300

    Melhorando o jogadas possiveis no mid game

--- ballsortsolver.cpp
+++ ballsortsolver.cpp
@@ -11,7 +11,8 @@ struct node
 };
 
 
-
+// acho que a heuristica tem que levar em conta a profundidade da ultima bola comparada a do topo,
+// quantidades de bolas de cores diferentes e a posicao das bolas de outras cores.
 int heuristica(list<int> provetas[], int numeroProvetas, int tamanhoProveta)
 {
     int total = 0;
@@ -19,9 +20,11 @@ int heuristica(list<int> provetas[], int numeroProvetas, int tamanhoProveta)
     {
         list<int>::iterator base = provetas[i].begin();
         int aux = *base;
-        for (list<int>::iterator it = base++; it != provetas[i].end(); ++it)
+        int j = 1;
+        for (list<int>::iterator it = provetas[i].end(); it != provetas[i].begin(); --it)
         {
-            total = total + abs(aux - *it);
+            total = total + abs(aux - *it)*j;
+            j++;
         }
     }
     return total;
@@ -33,20 +36,79 @@ void jogadasPossiveisInicio(list<int> provetas[], int numeroProvetas, int tamanh
     {
         if(provetas[i].empty() == true)
         {
-            for (int j = 0; j < tamanhoProveta; j++)
+            for (int j = 0; j < numeroProvetas; j++)
             {
-                jogadasPermitidas[i][j] = 1;                
+                jogadasPermitidas[j][i] = 1;                
             }
+            jogadasPermitidas[i][i] = 0;
         }
         for (int j = 0; j < numeroProvetas; j++)
         {
             if(provetas[i].back() == provetas[j].back() && i != j && provetas[j].size() != tamanhoProveta)
-                jogadasPermitidas[i][j] = 1;
+                jogadasPermitidas[j][i] = 1;
         }
     }
 }
+// jogada é um array com 2 valores i e j ([i,j]), a bola de i vai para j;
+// funçao para a atualizar aas jogdas permitidas  
+// talvez seja interessante pensar depois numa estrutura que nao considere os tubos ja feitos
+void jogadasPossiveisMID(list<int> provetas[], int numeroProvetas, int tamanhoProveta, int **jogadasPermitidas, int jogada[])
+{    
+    if(provetas[jogada[0]].back() != provetas[jogada[1]].back())
+    {
+        for (int j = 0; j < numeroProvetas; j++)
+        {
+            if(provetas[j].back() == provetas[jogada[0]].back() && j != jogada[0])
+            {
+                if(provetas[j].size() != tamanhoProveta)
+                {
+                    jogadasPermitidas[jogada[0]][j] = 1;    
+                    jogadasPermitidas[j][jogada[0]] = 1;
+                }
+                else
+                {
+                    jogadasPermitidas[j][jogada[0]] = 1;  
+                }
+            }
+            else
+            {
+                jogadasPermitidas[jogada[0]][j] = 0;
+                jogadasPermitidas[j][jogada[0]] = 0;  
+            }
+        }
 
-void PrintjogadasPossiveisInicio(int numeroProvetas, int **jogadasPermitidas)
+    }
+
+    // atualiza jogadasPermitidas se a proveta alvo da mudança encha com a mudança 
+    if(provetas[jogada[1]].size() == tamanhoProveta)
+    {
+        for (int j = 0; j < numeroProvetas; j++)
+        {
+            jogadasPermitidas[j][jogada[1]] == 0;
+        }
+    }
+
+    // atualiza jogadasPermitidas se a proveta origem da mudança esvazie com a mudança
+    if(provetas[jogada[0]].empty() == true)
+    {
+        for (int j = 0; j < numeroProvetas; j++)
+        {
+            jogadasPermitidas[jogada[0]][j] = 0;   
+            jogadasPermitidas[j][jogada[0]] = 1;             
+        }
+        jogadasPermitidas[jogada[0]][jogada[0]] = 0;
+    }
+}
+
+void jogou(list<int> provetas[], int numeroProvetas, int tamanhoProveta, int **jogadasPermitidas, int jogada[])
+{
+    int aux = provetas[jogada[0]].back();
+    provetas[jogada[0]].pop_back();
+    provetas[jogada[1]].push_back(aux);
+    jogadasPossiveisMID(provetas, numeroProvetas, tamanhoProveta, jogadasPermitidas, jogada);
+}
+
+void printjogadasPossiveis(int numeroProvetas, int **jogadasPermitidas)
 {
     for (int i = 0; i < numeroProvetas; i++)
     {
@@ -121,13 +183,19 @@ int main ()
     for (int i = 0; i < numeroProvetas; i++)
         jogadasPermitidas[i] = (int*)malloc(numeroProvetas*sizeof(int));
 
+    //mudar para char, pois nao tem diferença maior de uma bola da cor verde para uma de cor azul.
     list<int> provetas[numeroProvetas];
-
+    //tenho que passar esses parametros como ponteiros
     inicializar(provetas, numeroProvetas, tamanhoProveta);
     print(provetas, numeroProvetas, tamanhoProveta);
     cout << isOver(provetas, numeroProvetas, tamanhoProveta) << endl;
     jogadasPossiveisInicio(provetas, numeroProvetas, tamanhoProveta, jogadasPermitidas);
-    PrintjogadasPossiveisInicio(numeroProvetas, (int**)jogadasPermitidas);
-    return 0;
+    printjogadasPossiveis(numeroProvetas, jogadasPermitidas);     
+    int jogada[2] = {2,3};
+    jogou(provetas, numeroProvetas, tamanhoProveta, jogadasPermitidas, jogada);
+    print(provetas, numeroProvetas, tamanhoProveta);
+    printjogadasPossiveis(numeroProvetas, jogadasPermitidas);     
+    free(jogadasPermitidas);
+
 }
 
